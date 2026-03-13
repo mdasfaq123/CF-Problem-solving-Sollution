8e27dbfea40b26cdc9f242502100a6df01f63811
Md. Asfaq Ahbab Mayaz <mdasfaqahbabmayaz@gmail.com>
2026-03-13 09:36:57 +0600

    Add files via upload

---
+++ A_Deadline.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using ll = long long;
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define f0(i, n) for(ll i = 0; i < n; i++)
+#define f1(i, n) for(ll i = 1; i <= n; i++)
+#define testcase ll t; cin >> t; while(t--)
+#define testcase1 ll t; t = 1; while(t--)
+void solve()
+{
+    ll n, d; cin >> n >> d;
+    for(ll x = 0; x*x <= d; x++)
+    {
+        ll y = (d + x) / (x + 1);
+        if(x + y <= n)
+        {
+            cout << "YES" << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    testcase
+    {
+        solve();
+    }
+    return 0;
+}
+//Accepted
