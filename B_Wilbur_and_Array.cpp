decb024898ff9f69c76acfc7a65ae0e8d50e5d12
Md. Asfaq Ahbab Mayaz <mdasfaqahbabmayaz@gmail.com>
2026-03-20 19:58:42 +0600

    Add files via upload

---
+++ B_Wilbur_and_Array.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using ll = long long;
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define f0(i, n) for(ll i = 0; i < n; i++)
+#define f1(i, n) for(ll i = 1; i <= n; i++)
+#define testcase ll t; cin >> t; while(t--)
+#define testcase1 ll t; t = 1; while(t--)
+void solve()
+{
+    ll n; cin >> n;
+    vector < ll > v(n);
+    for(auto &x: v) cin >> x;
+    ll cnt = abs(v[0]);
+    for(ll i = 1; i < n; i++)
+    {
+        cnt += abs(v[i] - v[i-1]);
+    }
+    cout << cnt << endl;
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    testcase1
+    {
+        solve();
+    }
+    return 0;
+}
+// Accepted
