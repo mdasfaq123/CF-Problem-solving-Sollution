af84485782265dec70fbb7a6d1a25e2758f70e44
Md. Asfaq Ahbab Mayaz <mdasfaqahbabmayaz@gmail.com>
2026-03-22 00:05:23 +0600

    Add files via upload

---
+++ A_Flip_Flops.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using ll = long long;
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define f0(i, n) for(ll i = 0; i < n; i++)
+#define f1(i, n) for(ll i = 1; i <= n; i++)
+#define testcase ll t; cin >> t; while(t--)
+#define testcase1 ll t; t = 1; while(t--)
+void solve()
+{
+    ll n, c, k; cin >> n >> c >> k;
+    vector < ll > v(n+1);
+    for(ll i = 1; i <= n; i++) cin >> v[i];
+    sort(v.begin() + 1, v.begin() + 1 + n);
+    for(ll i = 1; i <= n; i++)
+    {
+        if(v[i] > c) break;
+        ll x = min(k, c - v[i]);
+        k -= x;
+        c += v[i] + x;
+    }
+    cout << c << endl;
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    testcase
+    {
+        solve();
+    }
+    return 0;
+}
+// Accepted
