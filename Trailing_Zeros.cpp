8eec75b16fa2c31c903a365249d69e717da26ba6
Md. Asfaq Ahbab Mayaz <mdasfaqahbabmayaz@gmail.com>
2026-05-18 20:21:21 +0600

    Add files via upload

---
+++ Trailing_Zeros.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define f0(i, n) for(ll i = 0; i < n; i++)
+#define f1(i, n) for(ll i = 1; i <= n; i++)
+#define testcase ll t; cin >> t; while(t--)
+#define testcase1 ll t; t = 1; while(t--)
+void solve()
+{
+    ll n; cin >> n;
+    ll cnt = 0;
+    for(ll i = 5; i <= n; i *= 5) cnt += (n / i);
+    cout << cnt << endl;
+}
+int32_t main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    testcase1
+    {
+        solve();
+    }
+    return 0;
+}
